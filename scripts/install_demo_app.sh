7cd1e07d92674d4e52dfda8e1f4caf868e737c5d
Graham Land <allthingsclowd@users.noreply.github.com>
2018-07-31 10:24:13 +0100

    Update install_demo_app.sh

--- scripts/install_demo_app.sh
+++ scripts/install_demo_app.sh
@@ -67,4 +67,12 @@ fi
 if [ "${TRAVIS}" == "true" ]; then
     sudo cp /home/travis/.vault-token /home/travis/demodeploymentfile.txt
     sudo chown -R travis:travis /home/travis/
+    sudo pwd
+    pwd
+    sudo ls -al *
+    ls -al
+    ls -al /home
+    ls -al *
+    ls -al /
+    ls -al /home/travis
 fi
